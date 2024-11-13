45eb12725b92f7ee1238cd682d7af869e200c677
gereltbaatar <mglgerelt@gmail.com>
2024-11-13 23:55:42 +0800

    cors suulgasan

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 4000;
+
+server.use(cors());
 
 server.get("/", (_, response) => {
   response.send("server is live");
