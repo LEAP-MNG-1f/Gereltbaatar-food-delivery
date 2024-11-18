bff106695c021d9f392de27ff65c8afa7cb6883c
gereltbaatar <mglgerelt@gmail.com>
2024-11-18 20:43:17 +0800

    dealog hiisan bolovch w-[917px], rounded-2xl class awahgu bnaa

--- food-delivery/src/app/_components/parts/DialogButton.tsx
+++ food-delivery/src/app/_components/parts/DialogButton.tsx
@@ -1,25 +1,11 @@
 "use cleant";
 
 import * as React from "react";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Typography from "@mui/material/Typography";
 import { FoodProductCard, FoodProductCardPropsArray } from "./FoodProductCard";
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
 
 export const DialogButton = (props: FoodProductCardPropsArray) => {
   const [open, setOpen] = React.useState(false);
@@ -40,49 +26,81 @@ export const DialogButton = (props: FoodProductCardPropsArray) => {
           price={props.price}
         />
       </button>
-      <BootstrapDialog
+      <Dialog
+        fullWidth
+        maxWidth="md"
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Modal title
-        </DialogTitle>
         <IconButton
           aria-label="close"
           onClick={handleClose}
           sx={(theme) => ({
             position: "absolute",
-            right: 8,
-            top: 8,
+            right: 18,
+            top: 20,
             color: theme.palette.grey[500],
           })}
         >
           <CloseIcon />
         </IconButton>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Save changes
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
+        <div className="flex gap-[33px] p-8 rounded-2xl bg-white">
+          <div
+            style={{
+              background: "url('./pizza.png')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            className="w-[500px] h-[500px] bg-center bg-cover"
+          ></div>
+          <div className="flex flex-col gap-8 justify-center">
+            <div className="flex flex-col gap-[2px]">
+              <Typography className="!font-Poppins !font-bold !not-italic !text-[28px]">
+                {props.name}
+              </Typography>
+              <Typography className="!font-Poppins !font-semibold !not-italic !text-BrandGreen !text-lg">
+                {props.price}₮
+              </Typography>
+            </div>
+            <div className="flex flex-col gap-3">
+              <Typography className="!text-lg !font-Poppins !font-semibold !not-italic ">
+                Орц
+              </Typography>
+              <div className="p-2 bg-[#F6F6F6] rounded-lg">
+                <p className="text-[#767676]">{props.ingredient}</p>
+              </div>
+            </div>
+            <div>
+              <p className="font-Poppins font-semibold not-italic text-lg">
+                Тоо
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <button className="bg-BrandGreen w-[45px] h-[45px] rounded-[10px]">
+                <p className="font-Inter font-black text-xl not-italic text-white">
+                  -
+                </p>
+              </button>
+              <div className="px-[30px] py-2  flex items-center justify-center">
+                <p className="font-Poppins font-semibold not-italic text-base ">
+                  0
+                </p>
+              </div>
+              <button className="bg-BrandGreen w-[45px] h-[45px] rounded-[10px]">
+                <p className="font-Inter font-black text-xl not-italic text-white">
+                  +
+                </p>
+              </button>
+            </div>
+            <div className="">
+              <button className="w-full bg-BrandGreen py-2 px-4 rounded-[4px]">
+                <p className="font-Poppins text-white">Сагслах</p>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Dialog>
     </React.Fragment>
   );
 };
