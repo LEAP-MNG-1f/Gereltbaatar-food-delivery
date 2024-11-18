6f67db8c7fd109615e3dbae7dee8dcb3981675e4
Гэрэлтбаатар Мягмар <mglgerelt@gmail.com>
2024-11-18 12:59:10 +0800

    Header fixed boson

--- food-delivery/src/app/_components/parts/Header.tsx
+++ food-delivery/src/app/_components/parts/Header.tsx
@@ -7,35 +7,37 @@ import { PineconeLogo } from "../svg/PineconeLogo";
 export const Header = () => {
   return (
     <header>
-      <div className="container m-auto">
-        <div className="flex justify-between py-2 px-6">
-          <div className="flex gap-6">
-            <PineconeLogo />
-            <div className="flex gap-2 items-center">
-              <button>
-                <Link href="/">
+      <div className="bg-white w-full fixed z-50">
+        <div className="container m-auto">
+          <div className="flex justify-between py-2 px-6">
+            <div className="flex gap-6">
+              <PineconeLogo />
+              <div className="flex gap-2 items-center">
+                <button>
+                  <Link href="/">
+                    <p className="px-4 py-2 font-bold text-sm leading-4 tracking-[-0.2px]">
+                      НҮҮР
+                    </p>
+                  </Link>
+                </button>
+                <button>
+                  <Link href="/menu">
+                    <p className="px-4 py-2 font-bold text-sm leading-4 tracking-[-0.2px]">
+                      ХООЛНЫ ЦЭС
+                    </p>
+                  </Link>
+                </button>
+                <button>
                   <p className="px-4 py-2 font-bold text-sm leading-4 tracking-[-0.2px]">
-                    НҮҮР
+                    ХҮРГЭЛТИЙН БҮС
                   </p>
-                </Link>
-              </button>
-              <button>
-                <Link href="/menu">
-                  <p className="px-4 py-2 font-bold text-sm leading-4 tracking-[-0.2px]">
-                    ХООЛНЫ ЦЭС
-                  </p>
-                </Link>
-              </button>
-              <button>
-                <p className="px-4 py-2 font-bold text-sm leading-4 tracking-[-0.2px]">
-                  ХҮРГЭЛТИЙН БҮС
-                </p>
-              </button>
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center gap-2 px-4 py-2">
-            <CartIcon />
-            <p className="font-bold text-sm tracking-[-0.3px]">Сагс</p>
+            <div className="flex items-center gap-2 px-4 py-2">
+              <CartIcon />
+              <p className="font-bold text-sm tracking-[-0.3px]">Сагс</p>
+            </div>
           </div>
         </div>
       </div>
