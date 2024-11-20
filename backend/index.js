041cd8e3069a3273242044b49c63ea7cba75fbff
Гэрэлтбаатар Мягмар <mglgerelt@gmail.com>
2024-11-20 12:53:52 +0800

    drawer components ugsrah

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 
--- food-delivery/src/app/_components/parts/CartDrawer.tsx
+++ food-delivery/src/app/_components/parts/CartDrawer.tsx
@@ -2,7 +2,9 @@
 
 import * as React from "react";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import Button from "@mui/material/Button";
+import { CartIcon } from "../svg/CartIcon";
+import { GetOutIcon } from "../svg/GetOutIcon";
+import { DrawerCartComponent } from "./DrawerCartComponent";
 
 type Anchor = "right";
 
@@ -21,14 +23,46 @@ export const CartDrawer = () => {
     <div>
       {(["right"] as const).map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>namaig hardaa</Button>
+          <button
+            className="flex items-center gap-2 px-4 py-2"
+            onClick={toggleDrawer(anchor, true)}
+          >
+            <CartIcon />
+            <p className="font-bold text-sm tracking-[-0.3px]">Сагс</p>
+          </button>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
             onOpen={toggleDrawer(anchor, true)}
           >
-            wedwefwesfwefewf
+            <div className="max-w-[586px] w-[586px] h-full">
+              <div className="flex flex-col justify-between gap-10 h-full">
+                <div className="">
+                  <div className="flex justify-between py-[26px] px-6 items-center">
+                    <button onClick={toggleDrawer(anchor, false)}>
+                      <GetOutIcon />
+                    </button>
+                    <p className="font-Poppins text-xl font-black">Таны сагс</p>
+                    <div></div>
+                  </div>
+                  <div className="h-full px-6">
+                    <DrawerCartComponent />
+                  </div>
+                </div>
+                <div className="grid grid-cols-2 items-center justify-between py-[10px] px-8 h-[172px] shadow-black shadow-2xl">
+                  <div className="">
+                    <p className="font-Poppins text-lg text-[#5E6166]">
+                      Нийт төлөх дүн
+                    </p>
+                    <p className="text-lg font-bold">34,800₮</p>
+                  </div>
+                  <div className="flex items-center justify-center bg-BrandGreen py-2 px-4 flex-[1_0_0] rounded-[4px]">
+                    <p className="font-Poppins text-white">Захиалах</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </SwipeableDrawer>
         </React.Fragment>
       ))}
