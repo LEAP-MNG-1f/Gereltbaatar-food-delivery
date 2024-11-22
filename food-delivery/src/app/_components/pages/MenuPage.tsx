294d12e0775b304e360d1cf2e4c4aa2ad4e5912f
gereltbaatar <mglgerelt@gmail.com>
2024-11-22 15:06:32 +0800

    fechdata hiisen

--- food-delivery/src/app/_components/pages/MenuPage.tsx
+++ food-delivery/src/app/_components/pages/MenuPage.tsx
@@ -5,151 +5,26 @@ import { Header } from "../feature/parts/Header";
 import { FoodProductCardPropsArray } from "../ui/cards/FoodProductCard";
 import { CategoryZone } from "../feature/zones/CategoryZone";
 import { MenuProductZone } from "../feature/zones/MenuProductZone";
+import { useEffect } from "react";
 
-const ProductArray: FoodProductCardPropsArray[] = [
-  {
-    _id: "1",
-    name: "Cheeseburger",
-    price: 5.99,
-    image: "./FoodImg.png",
-    ingredient: "Beef Patty, Cheese, Lettuce, Tomato, Bun",
-  },
-  {
-    _id: "2",
-    name: "Margherita Pizza",
-    price: 8.99,
-    image: "./FoodImg.png",
-    ingredient: "Tomato Sauce, Mozzarella, Basil, Olive Oil",
-  },
-  {
-    _id: "3",
-    name: "Chicken Caesar Salad",
-    price: 7.49,
-    image: "./FoodImg.png",
-    ingredient: "Chicken, Romaine Lettuce, Croutons, Parmesan, Caesar Dressing",
-  },
-  {
-    _id: "4",
-    name: "Spaghetti Bolognese",
-    price: 10.99,
-    image: "./FoodImg.png",
-    ingredient: "Spaghetti, Ground Beef, Tomato Sauce, Onion, Garlic",
-  },
-  {
-    _id: "5",
-    name: "Vegetable Stir Fry",
-    price: 6.99,
-    image: "./FoodImg.png",
-    ingredient: "Broccoli, Carrots, Bell Peppers, Soy Sauce, Ginger",
-  },
-  {
-    _id: "6",
-    name: "Grilled Salmon",
-    price: 12.99,
-    image: "./FoodImg.png",
-    ingredient: "Salmon Fillet, Lemon, Olive Oil, Garlic, Parsley",
-  },
-  {
-    _id: "7",
-    name: "Beef Tacos",
-    price: 4.99,
-    image: "./FoodImg.png",
-    ingredient: "Ground Beef, Taco Shells, Lettuce, Cheese, Salsa",
-  },
-  {
-    _id: "8",
-    name: "Avocado Toast",
-    price: 5.49,
-    image: "./FoodImg.png",
-    ingredient: "Avocado, Toast, Lemon Juice, Chili Flakes",
-  },
-  {
-    _id: "9",
-    name: "Chocolate Brownie",
-    price: 3.99,
-    image: "./FoodImg.png",
-    ingredient: "Cocoa, Sugar, Butter, Flour, Eggs",
-  },
-  {
-    _id: "10",
-    name: "Mango Smoothie",
-    price: 4.49,
-    image: "./FoodImg.png",
-    ingredient: "Mango, Yogurt, Milk, Honey",
-  },
-  {
-    _id: "11",
-    name: "French Fries",
-    price: 2.99,
-    image: "./FoodImg.png",
-    ingredient: "Potatoes, Salt, Oil",
-  },
-  {
-    _id: "12",
-    name: "BBQ Chicken Wings",
-    price: 7.99,
-    image: "./FoodImg.png",
-    ingredient: "Chicken Wings, BBQ Sauce, Garlic, Paprika",
-  },
-  {
-    _id: "13",
-    name: "Pancakes",
-    price: 4.99,
-    image: "./FoodImg.png",
-    ingredient: "Flour, Eggs, Milk, Butter, Maple Syrup",
-  },
-  {
-    _id: "14",
-    name: "Greek Salad",
-    price: 5.99,
-    image: "./FoodImg.png",
-    ingredient: "Cucumber, Tomato, Feta Cheese, Olives, Olive Oil",
-  },
-  {
-    _id: "15",
-    name: "Butter Chicken",
-    price: 11.99,
-    image: "./FoodImg.png",
-    ingredient: "Chicken, Butter, Tomato, Cream, Spices",
-  },
-  {
-    _id: "16",
-    name: "Veggie Burger",
-    price: 6.99,
-    image: "./FoodImg.png",
-    ingredient: "Veggie Patty, Lettuce, Tomato, Cheese, Bun",
-  },
-  {
-    _id: "17",
-    name: "Shrimp Tacos",
-    price: 9.99,
-    image: "./FoodImg.png",
-    ingredient: "Shrimp, Taco Shells, Cabbage, Sour Cream, Lime",
-  },
-  {
-    _id: "18",
-    name: "Apple Pie",
-    price: 5.49,
-    image: "./FoodImg.png",
-    ingredient: "Apples, Sugar, Butter, Flour, Cinnamon",
-  },
-  {
-    _id: "19",
-    name: "Chocolate Milkshake",
-    price: 3.99,
-    image: "./FoodImg.png",
-    ingredient: "Milk, Chocolate Syrup, Ice Cream",
-  },
-  {
-    _id: "20",
-    name: "Chicken Fried Rice",
-    price: 8.49,
-    image: "./FoodImg.png",
-    ingredient: "Rice, Chicken, Soy Sauce, Carrots, Egg",
-  },
-];
+const ProductArray: FoodProductCardPropsArray[] = [];
 
 const MenuPage = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/food-information");
+      const responsedata = await response.json();
+      const realData = responsedata.data;
+      console.log("MongoDB data:", realData);
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <main>
       <Header />
