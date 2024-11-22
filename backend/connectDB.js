c93cbae2b37be222f18ce05505b8416c7cd676cf
gereltbaatar <mglgerelt@gmail.com>
2024-11-22 15:08:44 +0800

    post huselt hiij mockdata post hiisen

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,16 +1,17 @@
 import { MongoClient } from "mongodb";
 const connectionString =
-  "mongodb+srv://ace12d192:wap3TZLOZeJlgLIq@cluster0.s6jvj.mongodb.net/";
-const connectDB = async () => {
-  const client = new MongoClient(connectionString);
-  let connection;
-  try {
-    connection = await client.connect();
-  } catch (e) {
-    console.error(e);
+  "mongodb+srv://mglgerelt:VmaUhczrxg9VHGmb@gereltbaatardata.1cupz.mongodb.net/";
+const connectiondb = async () => {
+  {
+    const client = new MongoClient(connectionString);
+    let connection;
+    try {
+      connection = await client.connect();
+    } catch (e) {
+      console.error(e);
+    }
+    let db = connection.db("food-delivery");
+    return db;
   }
-  let db = connection.db("sample_mflix");
-  return db;
 };
-
-export default connectDB;
+export default connectiondb;
