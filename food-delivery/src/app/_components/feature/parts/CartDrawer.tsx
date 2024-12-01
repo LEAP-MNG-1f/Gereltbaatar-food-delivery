7889d21832aa2bdcbd5129a54d2499edd55ac37a
gereltbaatar <mglgerelt@gmail.com>
2024-12-01 18:13:29 +0800

    push nest hiij boloh buh zuiliig hiisen

--- food-delivery/src/app/_components/feature/parts/CartDrawer.tsx
+++ food-delivery/src/app/_components/feature/parts/CartDrawer.tsx
@@ -80,18 +80,7 @@ export const CartDrawer = () => {
                     <p className="font-Poppins text-xl font-black">Таны сагс</p>
                     <div></div>
                   </div>
-                  <div className="2xl:h-[840px] h-[610px] px-6 overflow-scroll scrollbar-none">
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
-                    <DrawerCartCard />
+                  <div className="xl:h-[700px] h-[840px] px-6 overflow-scroll scrollbar-none">
                     <DrawerCartCard />
                     <DrawerCartCard />
                     <DrawerCartCard />
